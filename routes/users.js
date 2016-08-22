fe11a11cb5e414ab99cd7494717f4724d246a5d9
jeromeyang <jeeromeyang@gmail.com>
2016-08-22 17:08:08 +0800

    ad

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var json = "{'value':'http://120.25.168.81/images/welcome2.png','name':'welcome2','type':1}"
+
+
 /* GET users listing. */
-router.get('/yangjun', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/ad', function(req, res, next) {
+  res.send(json);
 });
 
 module.exports = router;
